57713296c1e8d3249cd857e53380a166500898e9
K80Git <67084301+K80Git@users.noreply.github.com>
2020-08-25 13:54:21 +0900

    running_test

---
+++ spec/factories/reviews.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+
+  factory :review do
+    shopName          {"abc"}
+    menuName          {"abc"}
+    kind              {"1"}
+    situation         {"1"}
+    deliciousness     {"1"}
+    costPerformance   {"1"}
+    sweetness         {"1"}
+    strongTaste       {"1"}
+    Texture           {"1"}
+    EaseOfEating      {"1"}
+    volume            {"1"}
+    appearance        {"1"}
+    balance           {"1"}
+    disagreement      {"1"}
+    group             {"1"}
+    comment           {"1"}
+    user_id           {"1"}
+  end
+
+end
